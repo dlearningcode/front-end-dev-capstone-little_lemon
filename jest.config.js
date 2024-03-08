f9b4e6148b1d5ed6898c124d3132378d8e10f378
Dion GeBorde <diongeborde@gmail.com>
2024-03-08 12:19:38 -0800

    Update Jest configuration and add tests for ReservationForm component

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,6 @@ module.exports = {
   moduleNameMapper: {
     "\\.(css|less|scss|sss|styl)$": "<rootDir>/node_modules/jest-css-modules",
     "\\.(jpg|jpeg|png|gif|webp|svg)$": "<rootDir>/fileMock.js",
+    testEnvironment: "jsdom",
   },
 };
